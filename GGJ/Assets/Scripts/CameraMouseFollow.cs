dce2f0a63428fa33962f03a54a95e1cc4a63f666
papayad <36908001+papayad@users.noreply.github.com>
2020-01-31 21:04:48 -0500

    Began coding mouse to follow camera
    UwU

---
+++ GGJ/Assets/Scripts/CameraMouseFollow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMouseFollow : MonoBehaviour
+{
+    public bool showCursor;
+    public float sensitivity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            if (showCursor == false)
+            {
+                Debug.Log("Mouse Visible");
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+                showCursor = true;
+            }
+            else if (showCursor == true)
+            {
+                Debug.Log("Mouse Invisible");
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+                showCursor = false;
+            }
+        }
+
+        float newRotationY = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivity;
+    }
+}
