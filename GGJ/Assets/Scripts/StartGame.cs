b5cc3e28b4d58ff9b76a79c413d39179aaceaab5
Leann Sterling <42954018+LeannSterling@users.noreply.github.com>
2020-02-01 18:58:04 -0500

    Start screen

---
+++ GGJ/Assets/Scripts/StartGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public void StartPlayingGame()
+    {
+        Debug.Log("Start");
+        SceneManager.LoadScene("Level00", LoadSceneMode.Single);
+    }
+}
