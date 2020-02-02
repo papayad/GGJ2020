035da0cd94cf7f19baa8b31fd009c1f79545b73f
Leann Sterling <42954018+LeannSterling@users.noreply.github.com>
2020-02-01 21:47:35 -0500

    Barks Added

---
+++ GGJ/Assets/Scripts/Barks.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Barks : MonoBehaviour
+{
+    public GameObject[] barkArray;
+
+    private int currentIndex = 0;
+
+    float elapsedTime = 0f; // Counts up to repeatTime
+    float repeatTime = 10f; // Time taken to repeat in seconds
+
+    void Start()
+    {
+        Debug.Log("The number of UI elements in the array is: " + barkArray.Length);
+    }
+
+    private void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime >= repeatTime) {
+            // Do something here
+            NewRandomObject();
+             // Subtract repeat time
+             elapsedTime -= repeatTime;
+        }
+    }
+
+        public void NewRandomObject()
+    {
+        int newIndex = Random.Range(0, barkArray.Length);
+        // Deactivate old gameobject
+        barkArray[currentIndex].SetActive(false);
+        // Activate new gameobject
+        currentIndex = newIndex;
+        barkArray[currentIndex].SetActive(true);
+    }
+}
